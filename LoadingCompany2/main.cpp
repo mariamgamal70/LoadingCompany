fe52c269ffd6d85f5c6757670d469f749ced2b1d
mariamgamal70 <mariamgamal70@gmail.com>
2022-05-03 23:28:55 +0200

    all cleared out

--- LoadingCompany2/main.cpp
+++ LoadingCompany2/main.cpp
@@ -5,8 +5,5 @@ int main()
 {
 	CompanyClass Maestro;
 	Maestro.SimulatorFunction();
-	cout << "this is a new branch trying it out" << endl;
-	cout << "pushing branch into origin" << endl;
-	cout << "this is a new branch trying it out" << endl;
-	cout << "add new change" << endl;
+
 }
