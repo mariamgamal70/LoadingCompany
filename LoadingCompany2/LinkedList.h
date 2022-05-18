3ddb27a2d0a736586bb24d4964c98a4ce3defe8e
mariamgamal70 <mariamgamal70@gmail.com>
2022-05-18 20:18:51 +0200

    adjusted counters and removed extra comments n old template specializations

--- LoadingCompany2/LinkedList.h
+++ LoadingCompany2/LinkedList.h
@@ -31,7 +31,6 @@ public:
 		Node<T>* R = new Node<T>(data);
 		R->setNext(Head);
 		Head = R;
-		count++;
 	}
 
 	void DeleteAll()//Deletes all nodes of the list.
@@ -43,6 +42,7 @@ public:
 			delete Head;
 			Head = P;
 		}
+		count = 0;
 	}
 	/*void InsertEnd(const T& data) //Create new node at the end of the list
 	{
@@ -81,23 +81,9 @@ public:
 			R->setNext(nullptr);
 			P->setNext(R);
 		}
+		count++;
 	}
-	/*void InsertEnd(T data)
-	{
-		Node<T>* R = new Node<T>(data);
-		if (!Head)
-		{
-			Head = R;
-			return;
-		}
 
-		Node<T>* ptr = Head;
-		while (ptr->getNext() != nullptr)
-		{
-			ptr = ptr->getNext();
-		}
-		ptr->setNext(R);
-	}*/
 	void printList()
 	{
 		Node<T>* curr = Head;
@@ -113,11 +99,8 @@ public:
 			cout << curr->getItem() << ","<<" ";
 
 			}
-			
 			curr = curr->getNext();
-
 		}
-		//cout << endl;
 	}
 
 	bool peek(Node<T>& c)
@@ -144,6 +127,7 @@ public:
 				delete temp;
 				temp = NULL;
 				Head = NULL;
+				count--;
 			}
 		}
 		else
@@ -156,6 +140,7 @@ public:
 					temp->setNext(curr->getNext());
 					delete curr;
 					curr = nullptr;
+					count--;
 					break;
 				}
 				temp = curr;
@@ -236,205 +221,5 @@ public:
 			return false;
 	}
 };
-/*
-class Cargo;
-template<>
-class  LinkedList <Cargo*>
-{
-private:
-	Node<Cargo*>* Head;	//Pointer to the head of the list
-	int count;
-public:
 
-	LinkedList()
-	{
-		Head = NULL;
-		count = 0;
-	}
-	~LinkedList()//List is being desturcted ==> delete all items in the list
-	{
-		DeleteAll();
-	}
-	void InsertBeg(Cargo* data)//Creates a new node and adds it to the beginning of a linked list,
-	//data : The value to be stored in the new node.
-
-	{
-		Node<Cargo*>* R = new Node<Cargo*>(data);
-		R->setNext(Head);
-		Head = R;
-		count++;
-	}
-
-	void DeleteAll()//Deletes all nodes of the list.
-	{
-		Node<Cargo*>* P = Head;
-		while (Head)
-		{
-			P = Head->getNext();
-			delete Head;
-			Head = P;
-		}
-	}
-	void InsertEnd(Cargo* data) //Create new node at the end of the list
-	{
-		Node<Cargo*>* R = new Node<Cargo*>(data);
-		Node<Cargo*>* curr = Head;
-		if (!Head)
-		{
-			Head = R;
-			R->setNext(NULL);
-		}
-		else
-		{
-			while (curr->getNext() != NULL)
-			{
-				curr = curr->getNext();
-			}
-			curr->setNext(R);
-			R->setNext(NULL);
-		}
-		count++;
-	}
-
-
-	void printList()
-	{
-
-		Node<Cargo*>* curr = Head;
-		while (curr != nullptr)
-		{
-			cout << curr->getItem() ;
-			curr = curr->getNext();
-
-		}
-		cout << endl;
-	}
-
-
-	bool peek(Cargo*& c)
-	{
-		if (!Head)
-			return false;
-		else
-		{
-			c = Head->getItem();
-			return true;
-		}
-	}
-
-	void DeleteSpecificNode(const int& value)
-	{
-		Node<Cargo*>* temp = Head;
-		if (temp == nullptr)
-			return;
-
-		if (temp->getNext() == NULL)
-		{
-			if (temp->getItem()->getCargoID() == value)
-			{
-				delete temp;
-				temp = NULL;
-				Head = NULL;
-			}
-		}
-
-		else
-		{
-			Node<Cargo*>* curr = temp->getNext();
-			while (curr)
-			{
-				if (curr->getItem()->getCargoID() == value)
-				{
-					temp->setNext(curr->getNext());
-					delete curr;
-					curr = nullptr;
-					break;
-
-				}
-				temp = curr;
-				curr = curr->getNext(); //FY warning hena ya sherif
-			}
-		}
-
-	}
-
-	void DeleteBeg()//---------------------->delete last node instead?
-	{
-		if (!Head)
-			return;
-		Node<Cargo*>* temp = Head;
-		if (Head->getNext() != nullptr)
-		{
-			Head = Head->getNext();
-			delete temp;
-			temp = nullptr;
-		}
-		else
-		{
-			delete Head;
-			Head = nullptr;
-		}
-	}
-
-	void DeleteEnd()//---------------------->delete last node instead?
-	{
-		if (!Head)
-			return;
-
-		Node<Cargo*>* prev = Head;
-		if (Head->getNext() == NULL)
-		{
-			delete Head;
-			Head = NULL;
-		}
-
-		else {
-
-			Node<Cargo*>* curr = prev->getNext();
-			while (curr->getNext() != NULL)
-			{
-				prev = curr;
-				curr = curr->getNext();
-			}
-			prev->setNext(curr->getNext());
-			delete curr;
-			curr = NULL;
-		}
-	}
-
-	int getCount()
-	{
-		int count = 0;
-		Node <Cargo*>* temp = Head;
-		while (temp)
-		{
-			count++;
-			temp = temp->getNext();
-		}
-		return count;
-	}
-	Cargo* getLastNode()
-	{
-		if (!Head)
-			return nullptr;
-
-		if (Head->getNext() == NULL)
-			return(Head->getItem());
-
-		Node<Cargo*>* curr = Head;
-		while (curr->getNext() != nullptr)
-		{
-			curr = curr->getNext();
-		}
-		return (curr->getItem());
-	}
-	bool isEmpty()
-	{
-		if (!Head)
-			return true;
-		else
-			return false;
-	}
-};
-*/
 #endif
